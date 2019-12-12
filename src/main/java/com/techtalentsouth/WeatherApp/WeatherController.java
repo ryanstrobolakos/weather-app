d7152ce65198499431af0dfcaa4d60a94e3acda2
Ryan Strobolakos <ryanstrobolakos@Ryans-MacBook-Air.local>
2019-12-12 08:42:02 -0500

    creating Weather Controller with get method to return index page

---
+++ src/main/java/com/techtalentsouth/WeatherApp/WeatherController.java
@@ -0,0 +1,21 @@
+package com.techtalentsouth.WeatherApp;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class WeatherController {
+	
+	@Autowired
+    private WeatherService weatherService;
+	
+	@GetMapping
+	public String getIndex(Model model) {
+	    Response response = weatherService.getForecast("43210");
+	    model.addAttribute("data", response);
+	    return "index";
+	}
+
+}
